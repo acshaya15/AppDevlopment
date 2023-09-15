2a3ebc591503b0bd8a2b3bcab954da8c1726fbbb
acshaya15 <126309334+acshaya15@users.noreply.github.com>
2023-09-15 16:15:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as Components from './Components';
+import {Sign} from './signUp';
+import  './style.css';
+function App() {
+    return(
+<Sign />
+    )
+}
+
+export default App;
---
+++ src/signUp.js
@@ -0,0 +1,57 @@
+import * as Components from './Components';
+import React ,{useState}from 'react';
+import  './style.css';
+import potato from './image/pngtree-cartoon-anthropomorphic-vegetable-ingredients-potato-free-illustration-image_1167264.jpg'
+export const Sign = () =>  {
+const [signIn, toggle] = React.useState(true);
+     return(
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                     <Components.Title>Create Account</Components.Title>
+                     <Components.Input type='text' placeholder='Name' />
+                     <Components.Input type='email' placeholder='Email' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Components.Button>Sign Up</Components.Button>
+                 </Components.Form>
+             </Components.SignUpContainer>
+
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Sign in</Components.Title>
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                      <Components.Button>Sigin In</Components.Button>
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+                 <Components.LeftOverlayPanel signinIn={signIn}>
+                     <Components.Title>Welcome Back Potato!</Components.Title>
+                     <Components.Paragraph>
+                     Life's too short not to have fun potato conversations.So resume your jouney with us.
+                     </Components.Paragraph>
+                     <Components.GhostButton onClick={() => toggle(true)}>
+                         Sign In
+                     </Components.GhostButton>
+                     </Components.LeftOverlayPanel>
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                       <Components.Title>PoTaTo</Components.Title>
+                       <Components.Paragraph>
+                       Fry-yay! It's potato time.When life gives you potatoes, share them with your friends!
+                       </Components.Paragraph>
+                           <Components.GhostButton onClick={() => toggle(false)}>
+                               Sigin Up
+                           </Components.GhostButton> 
+                     </Components.RightOverlayPanel>
+ 
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+     )
+     }
