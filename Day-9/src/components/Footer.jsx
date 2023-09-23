bf5b9530d7419ccae94d905310e24381b30dd915
acshaya15 <126309334+acshaya15@users.noreply.github.com>
2023-09-23 12:12:14 +0530

    Delete Day-9 directory

--- Day-9/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = ()  =>{
-    return(
-        <footer className="footer">
-          <h1 className="abtus">AboutUs</h1>
-            <h1 className="contus">ContactUs</h1>
-        </footer>
-    )
-}
-export default Footer;
