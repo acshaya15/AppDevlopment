1a79c0efa69fbb21b502ae9b083faceae130eb8e
acshaya15 <126309334+acshaya15@users.noreply.github.com>
2023-09-23 12:09:35 +0530

    Add files via upload

---
+++ Day-9/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = ()  =>{
+    return(
+        <footer className="footer">
+          <h1 className="abtus">AboutUs</h1>
+            <h1 className="contus">ContactUs</h1>
+        </footer>
+    )
+}
+export default Footer;
