2cba68f2476058aa3fe4bbf78ff64aa23607073b
acshaya15 <126309334+acshaya15@users.noreply.github.com>
2023-10-09 15:29:07 +0530

    Add files via upload

---
+++ Day-13/src/components/centerimages.jsx
@@ -0,0 +1,94 @@
+// import React from 'react';
+// import thumbnail1 from "./images/thumbnail-1.webp";
+// function centerimages() {
+//   return (
+//     <div>
+//         <div class="video-grid-info">
+//             <div class="video-preview">
+//                 <div class="thumbnail-row">
+//                     <img class="image"  src={thumbnail-1.webp}>
+//                         <div class="video-time">
+//                             14.20
+//                         </div>
+//                 </div>
+            
+                
+//                 <div class="channel-picture">
+//                     {/* <img class="profile-picture" src="channel images/channel-1.jpeg"> */}
+//                 </div>
+        
+//                 <div class="video-texts">
+//                     <p class="video-title">
+//                         Talking Tech and AI with Google CEO Sundar Pichai!
+//                     </p>
+//                     <p class="video-author">
+//                             Marques Brownlee
+//                     </p>
+//                     <p class="video-stats">
+//                             3.4M views &#183;  6 months ago
+//                     </p>
+                    
+//                 </div>
+//             </div>
+            
+//         <div class="video-preview">
+//             <div class="thumbnail-row">
+//                 <img class="image"  src="thumbnail-2.webp">
+//                 <div class="video-time">
+//                     8:22
+//                 </div>
+//             </div>
+            
+//                 <div class="channel-picture">
+//                     <img class="profile-picture" src="channel images/channel-2.jpeg">
+//                 </div>
+        
+                
+//                 <div class="video-texts">
+//                     <p class="video-title">
+//                     Try Not To Laugh Challenge #9
+//                     </p>
+//                     <p class="video-author">
+//                         Markiplier   
+//                     </p>
+//                     <p class="video-stats">
+//                         19M views &#183; 4 years ago   
+//                     </p>
+//                 </div>
+            
+//         </div> 
+        
+//         <div class="video-preview">
+//             <div class="thumbnail-row">
+//                 <img class="image"  src="thumbnail-3.webp">
+//                 <div class="video-time">
+//                     9:13
+//                 </div>
+//             </div>
+            
+//                 <div class="channel-picture">
+//                     <img class="profile-picture" src="channel images/channel-3.jpeg">
+//                 </div>
+        
+                
+//                 <div class="video-texts">
+//                     <p class="video-title">
+                    
+//                         Crazy Tik Toks Taken Moments Before DISASTER
+                    
+//                     </p>
+//                     <p class="video-author">
+//                         SSSniperWolf  
+//                     </p>
+//                     <p class="video-stats">
+//                         12M views &#183; 1 year ago   
+//                     </p>
+//                 </div>
+            
+//         </div> 
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default centerimages
