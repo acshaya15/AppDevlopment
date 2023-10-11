3ac0772e002c16589b7c7496af2d1ad900afb052
acshaya15 <126309334+acshaya15@users.noreply.github.com>
2023-10-11 11:56:11 +0530

    Add files via upload

---
+++ Day-16/springclouddeploy/springclouddeploy/src/test/java/com/example/springclouddeploy/SpringclouddeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springclouddeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringclouddeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
